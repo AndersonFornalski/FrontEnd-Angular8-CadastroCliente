15a2dd0be880adb28c570e881938f08a7a094c27
root <AndersonFornalski>
2020-07-10 15:42:40 -0300

    mascaras para campos de input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { NgxMaskModule, IConfig } from 'ngx-mask';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,9 @@ import { HeadersComponent } from './components/header/headers/headers.component'
 import { CadastroClienteComponent } from './components/cliente/cadastro/cadastro-cliente/cadastro-cliente.component';
 import { ListaClienteComponent } from './components/cliente/cadastro/lista-cliente/lista-cliente.component';
 import { ClienteService } from './components/services/cliente.service';
+
+//para mascaras no html
+ const options: Partial<IConfig> | (() => Partial<IConfig>) = {};
 
 @NgModule({
   declarations: [
@@ -21,6 +25,8 @@ import { ClienteService } from './components/services/cliente.service';
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    NgxMaskModule.forRoot(options),
+    
 
     AppRoutingModule
   ],
