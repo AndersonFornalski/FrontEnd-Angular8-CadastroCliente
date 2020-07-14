cb94fdfcf5d6d1fa1cf32a5ca6b60f3c9b159832
root <AndersonFornalski>
2020-07-14 09:46:54 -0300

    trocando Variavel __dirname, pelo caminho correto

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(__dirname + 'dist/ProjetoEntrevistCadastroCliente'));
+app.use(express.static("/home/theblackred/Documents/Angular/ProjetoEntrevistCadastroCliente/dist/ProjetoEntrevistCadastroCliente"));
 
 app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname + 'dist/ProjetoEntrevistCadastroCliente/index.html'));
+    res.sendFile(path.join(__dirname + '/home/theblackred/Documents/Angular/ProjetoEntrevistCadastroCliente/dist/ProjetoEntrevistCadastroCliente/index.html'));
 });
 
 app.listen(process.env.PORT || 8081),function(){
-    console.log("rodando na porta 4201")
+    console.log("rodando na porta 8081")
 }
