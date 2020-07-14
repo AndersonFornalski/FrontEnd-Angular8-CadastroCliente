3b98a9b02aba0a7a1f3d3f06720ca716d0f1c0f4
root <AndersonFornalski>
2020-07-14 10:14:17 -0300

    atualizando ng build para o heroku

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static("/home/theblackred/Documents/Angular/ProjetoEntrevistCadastroCliente/dist/ProjetoEntrevistCadastroCliente"));
+app.use(express.static("/ProjetoEntrevistCadastroCliente/dist/ProjetoEntrevistCadastroCliente"));
 
 app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname + '/home/theblackred/Documents/Angular/ProjetoEntrevistCadastroCliente/dist/ProjetoEntrevistCadastroCliente/index.html'));
+    res.sendFile(path.join('/home/theblackred/Documents/Angular/ProjetoEntrevistCadastroCliente/dist/ProjetoEntrevistCadastroCliente/index.html'));
 });
 
 app.listen(process.env.PORT || 8081),function(){
